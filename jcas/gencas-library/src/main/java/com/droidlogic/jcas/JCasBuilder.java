66789bfaf142ec46a609174f5d624e5824fa78d9
Kai.Song <kai.song@amlogic.com>
2024-01-30 12:55:32 +0000

    JCas: CF1 Support PIP Playback [1/1]
    PD#SWPL-154451
    
    Problem:
    JCas needs to verify PIP support based on JCas Demo
    
    Solution:
    Verify PIP playback support based on JCas Demo
    
    Verify:
    Android-U OHM
    
    Build Tag:
    Android_U_DRM_PATCHBUILD-2063
    
    Commits:
    vendor/amlogic/common/JCasLib:
    version: 1.1.3-g0c15fce(build:2063)
    0c15fce JCas: CF1 Support PIP Playback [1/1]
    
    Git Hash-Object:
    d3d61a5689e57a861f86a8ae6a0a3ddb1bdb99b8
    jcas/arm/libjcas_jni.so
    eea74ff2bd7800195520b92562b4e8770f3de13d
    jcas/arm/libjcas_native_client.so
    3c5382aede127ce620c1631aaab5e496f5db0e68
    jcas/gencas-library/libs/jcas-library.jar
    d3897a33d71d0d9a3621f4ab6e8da62faecfd091
    jcas/gencas-library/libs/jcas-widevine-library.jar
    
    Change-Id: I56a0ea3c03d2b1b973cfbd7bc4957ade54c473a5
    Signed-off-by: DRM Auto Build <tao.guo@amlogic.com>

--- jcas/gencas-library/src/main/java/com/droidlogic/jcas/JCasBuilder.java
+++ jcas/gencas-library/src/main/java/com/droidlogic/jcas/JCasBuilder.java
@@ -1,7 +1,6 @@
 package com.droidlogic.jcas;
 
 import static com.droidlogic.jcas.CasManager.EMM_FILTERING_ON_TUNE;
-import static com.droidlogic.jcas.CasManager.START_CAS_SERVICE_ON_BOOT;
 
 import com.droidlogic.jcas.vendor.widevine.WidevineCasProjectListener;
 import com.droidlogic.jcas.vendor.widevine.WidevineCasWrapperFactory;
@@ -12,7 +11,7 @@ import com.droidlogic.jcas.vendor.widevine.WidevineCasWrapperFactory;
 public class JCasBuilder {
     /**
      * Project can choose each Cas vendor's CasWrapperFactory in project library.
-     * ex) If WidevineCas case, need to change
+     * ex) If not WidevineCas case, need to change
      * 'WidevineCasWrapperFactory' to '{vendor}CasWrapperFactory'
      */
     public static CasWrapperFactory[] createFactory(String config) {
@@ -21,7 +20,7 @@ public class JCasBuilder {
 
     /**
      * Project can choose a each Cas vendor's CasProjectListener in project library.
-     * ex) If WidevineCas case, need to change
+     * ex) If not WidevineCas case, need to change
      * 'WidevineCasProjectListener' to '{vendor}CasProjectListener'
      */
     public static CasProjectListener createListener() {
@@ -29,13 +28,10 @@ public class JCasBuilder {
     }
 
     /**
-     * Project can choose a Emm filtering mode.
+     * Get the Emm filtering mode.
      * EMM_FILTERING_ON_TUNE:
      *  - EMM filtering lifetime is same as each Channel
      *  - It used only live connection to send CAT/EMM data.
-     * EMM_FILTERING_ALWAYS:
-     *  - EMM filtering lifetime is same as JCas Service
-     *  - It used only default connection using defaultCasWrapper to send CAT/EMM data.
      *
      * Default Mode: EMM_FILTERING_ON_TUNE
      */
@@ -50,27 +46,5 @@ public class JCasBuilder {
      */
     public static boolean getCasContentRatingEnabled() {
         return false;
-    }
-
-    /**
-     * The Project can choose when to start CasService.
-     *
-     * START_CAS_SERVICE_ON_BOOT:
-     * The casService started when received the BOOT_COMPLETED intent.
-     *
-     * START_CAS_SERVICE_ON_REQUEST:
-     * The casService started when receiving the below intent from the TV app.
-     *      - Intent name: com.droidlogic.jcas.ACTION_START_CAS_SERVICE
-     *      - Permission: com.droidlogic.jcas.permission.START_CAS_SERVICE
-     *
-     * Example) Intent startCasService = new Intent(INTENT_START_CAS_SERVICE);
-     *          startCasService.setPackage("com.google.android.tv.dtvinput");
-     *          Context.sendBroadcast(startCasService,
-     *          "com.droidlogic.jcas.permission.START_CAS_SERVICE");
-     *
-     * Default mode: START_CAS_SERVICE_ON_BOOT
-     */
-    public static int getStartCasServiceMode() {
-        return START_CAS_SERVICE_ON_BOOT;
     }
 }
